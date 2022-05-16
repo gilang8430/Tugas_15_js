dc33b9defc56d722bbe3f500a696967c82b046d0
gilang_gans <gilangsos99@gmail.com>
2022-05-16 13:38:22 +0700

    Tugas 15 js
    Tugas ini untuk niomic

---
+++ javascript/tugas15.js
@@ -0,0 +1,6 @@
+var AB = 64;
+var BC = 36;
+var AC = 10;
+console.log("Nilai AB :", Math.sqrt(AB));
+console.log("Nilai BC :", Math.sqrt(BC));
+console.log("panjang sisi AC pada Segitiga siku-siku tersebut adalah :", Math.pow(AC,1),"cm")
